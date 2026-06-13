a72fdfda743c0fe327c07d0ed7e52be87caa4587
adahirsmemorymural <edgardiazayala62@gmail.com>
2026-06-13 05:42:43 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,8 +2,10 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
+  addDoc,
   onSnapshot,
   updateDoc,
+  deleteDoc,
   doc
 } from "firebase/firestore";
 
@@ -18,7 +20,7 @@ const firebaseConfig = {
   apiKey: "TU_API_KEY",
   authDomain: "TU_AUTH_DOMAIN",
   projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
+  storageBucket: "TU_PROJECT_ID.appspot.com",
   messagingSenderId: "TU_SENDER_ID",
   appId: "TU_APP_ID"
 };
@@ -30,8 +32,10 @@ export const storage = getStorage(app);
 
 export {
   collection,
+  addDoc,
   onSnapshot,
   updateDoc,
+  deleteDoc,
   doc,
   ref,
   uploadBytes,
